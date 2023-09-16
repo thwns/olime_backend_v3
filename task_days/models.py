9399f66729da7b39ecdf9bcc1da3c3f35da7ef8c
username <email@email.com>
2023-09-16 14:52:12 +0900

    mvp fix v1

--- task_days/models.py
+++ task_days/models.py
@@ -7,8 +7,6 @@ class Task_Day(CommonModel):
     subject = models.CharField(max_length=150)
     title = models.CharField(max_length=150)
     to_do = models.CharField(max_length=150)
-    user = models.ManyToManyField(
-        "users.User", related_name="task_days")
 
     def __str__(self):
         return self.title
--- task_days/serializers.py
+++ task_days/serializers.py
@@ -7,23 +7,3 @@ class Task_DaySerializer(serializers.ModelSerializer):
     class Meta:
         model = Task_Day
         fields = '__all__'
-
-    def create(self, validated_data):
-        users_data = validated_data.pop('user')
-        task_day = Task_Day.objects.create(**validated_data)
-        for user_data in users_data:
-            task_day.user.add(user_data)
-        return task_day
-
-    def update(self, instance, validated_data):
-        users_data = validated_data.pop('user')
-        instance.subject = validated_data.get('subject', instance.subject)
-        instance.title = validated_data.get('title', instance.title)
-        instance.to_do = validated_data.get('to_do', instance.to_do)
-
-        instance.user.clear()
-        for user_data in users_data:
-            instance.user.add(user_data)
-
-        instance.save()
-        return instance
