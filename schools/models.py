cda207ad15bd5ca02b305696c3a3e450460d6c79
username <email@email.com>
2023-10-12 20:43:26 +0900

    change address

--- schools/models.py
+++ schools/models.py
@@ -7,7 +7,7 @@ class School(CommonModel):
     """ School Categories """
 
     name = models.CharField(max_length=50)
-    address = models.CharField(max_length=50)
+    address = models.CharField(max_length=500)
 
     def __str__(self):
         return f"{self.school}: {self.name}"
